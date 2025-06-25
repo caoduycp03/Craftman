db69dbc64c4382d3a0d10a6c21cbeceabd3f5bee
Ta Van Dung <v.dungtv41@vinai.io>
2025-06-25 15:51:17 +0000

    Quick commit

--- train_diffusion.sh
+++ train_diffusion.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=0
 python train.py --config ./configs/image-to-shape-diffusion/clip-dinov2-pixart-diffusion-dit32.yaml --train --gpu 0
 
 # # for multi view conditioned generation (original paper)
-# python train.py --config ./configs/image-to-shape-diffusion/clip-mvrgb-modln-l256-e64-ne8-nd16-nl6.yaml --train --gpu 0, 1
+# python train.py --config ./configs/image-to-shape-diffusion/clip-mvrgb-modln-l256-e64-ne8-nd16-nl6.yaml --train --gpu 0
 
 # # for DoraVAE single view version
 # python train.py --config ./configs/image-to-shape-diffusion/DoraVAE-dinov2reglarge518-pixart-rectified-flow-dit32.yaml --train --gpu 0
