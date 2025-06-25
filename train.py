6342aff5c3b9bc72d3f6a3948cc72da8e819a10a
Ta Van Dung <v.dungtv41@vinai.io>
2025-06-25 16:19:46 +0000

    Quick commit

--- train.py
+++ train.py
@@ -245,7 +245,6 @@ def main(args, extras) -> None:
     #     **cfg.trainer
     #     # profiler="pytorch",
     # )
-    breakpoint()
     trainer = Trainer(
         strategy=DeepSpeedStrategy(config="deepspeed_config.json"),
         accelerator="gpu",
