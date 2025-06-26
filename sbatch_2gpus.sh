cc71d15d37bbf08db6d091f4d0b588c2d4384b6d
Ta Van Dung <v.dungtv41@vinai.io>
2025-06-26 15:37:10 +0000

    Quick commit

--- sbatch_2gpus.sh
+++ sbatch_2gpus.sh
@@ -16,3 +16,13 @@ conda activate /lustre/scratch/client/movian/research/users/duycd/.conda/envs/Cr
 
 export CUDA_HOME=/lustre/scratch/client/movian/research/users/ducvh5/cuda-12.4
 python train.py --config ./configs/image-to-shape-diffusion/DoraVAE-dinov2reglarge518-pixart-rectified-flow-dit32-class-conditioned.yaml --train --gpu 0,1
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --master_port=23432 \
+    --master_addr="127.0.0.1" \
+    train.py \
+    --config ./configs/image-to-shape-diffusion/DoraVAE-dinov2reglarge518-pixart-rectified-flow-dit32-class-conditioned.yaml \
+    --train \
+    --gpu 0,1
