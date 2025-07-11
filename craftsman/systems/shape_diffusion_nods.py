a33ac889c1e5dc5a7aabfffb51b9fd7e95a2d593
Ta Van Dung <v.dungtv41@vinai.io>
2025-07-11 12:47:57 +0000

    Quick commit

--- craftsman/systems/shape_diffusion_nods.py
+++ craftsman/systems/shape_diffusion_nods.py
@@ -95,7 +95,6 @@ class ShapeDiffusionSystem(BaseSystem):
             device=latents.device,
         )
         timesteps = timesteps.long()
-        breakpoint()
         # 5. add noise
         noisy_z = self.noise_scheduler.add_noise(latents, noise, timesteps)
         # 6. diffusion model forward
@@ -138,7 +137,7 @@ class ShapeDiffusionSystem(BaseSystem):
                 mse_loss_weights = mse_loss_weights / snr
             elif self.noise_scheduler.config.prediction_type == "v_prediction":
                 mse_loss_weights = mse_loss_weights / (snr + 1)
-            
+            breakpoint()
             if self.cfg.loss.loss_type == "l1":
                 loss = F.l1_loss(noise_pred, target, reduction="none")
             elif self.cfg.loss.loss_type in ["mse", "l2"]:
